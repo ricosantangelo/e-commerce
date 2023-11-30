d555abc3721367bf563ae5ceb059ee09cae20c4c
Rico <ricosantangelo@gmail.com>
2023-11-30 12:38:07 -0800

    edited various files

--- models/index.js
+++ models/index.js
@@ -1,20 +1,37 @@
-// import models
-const Product = require('./Product');
+const Sequelize = require('sequelize');
+const sequelize = require('../config/connection');
+
+// Import models
 const Category = require('./Category');
+const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Set up associations
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
+// Export models and connection
 module.exports = {
-  Product,
+  sequelize,
   Category,
+  Product,
   Tag,
-  ProductTag,
+  ProductTag
 };
